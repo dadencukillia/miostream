2fd816c3a9014043649be363ee9ffdecd8be29db
Illia Diadenchuk <dadencukillia@gmail.com>
2026-09-13 20:28:15 +0300

    refactor: project development environment

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "A Nix devShell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        git
+        gnumake
+
+        docker
+        docker-compose
+        docker-buildx
+
+        bun
+        biome
+      ];
+
+      shellHook = ''
+        echo Welcome to devShell!
+      '';
+    };
+  };
+}
+
