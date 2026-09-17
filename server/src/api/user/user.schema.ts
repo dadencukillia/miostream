f0d3344417d305f89bd23bc234d55101c77d3165
ApostolOleg <apostololeg0@gmail.com>
2026-09-17 17:36:51 +0300

    feat: add schema for creating new users

--- server/src/api/user/user.schema.ts
+++ server/src/api/user/user.schema.ts
@@ -8,10 +8,46 @@ const userSchema = {
         email:      { type: 'string', format: 'email' },
         bio:        { type: 'string' },
         avatar_url: { type: 'string', format: 'uri'  },
-        social_networks:    { type: 'array', items: { type: 'string' } },
+        social_networks:    { type: 'array', items: { type: 'string', format: 'uri' } },
         current_streak:     { type: 'integer' },
         max_streak:         { type: 'integer' },
         profile_frame:      { type: 'string', format: 'uri' },
         profile_background: { type: 'string', format: 'uri' }
+    }
+}
+
+// Schema for creating a new user
+const createUserSchema = {
+    body: { 
+        type: 'object',
+        required: [
+            'nickname', 
+            'name',
+            'email',
+            'password',
+        ],
+        properties: {
+            nickname:   { 
+                type: 'string',
+                minLength: 3,
+                maxLength: 30
+            },
+            name:       {
+                type: 'string',
+                minLength: 3,
+                maxLength: 100
+            },
+            email:      { 
+                type: 'string',
+                format: 'email' 
+            },
+            password:   {
+                type: 'string' 
+            }
+        },
+        additionalProperties: false
+    },
+    response: {
+        201: userSchema
     }
 }
