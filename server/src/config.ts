22ef7d63dbb797a015093afe565bdb54da066c08
Illia Diadenchuk <dadencukillia@gmail.com>
2026-09-17 16:47:35 +0300

    feat: rustfs secret and access keys forwarding to backend

--- server/src/config.ts
+++ server/src/config.ts
@@ -6,3 +6,6 @@ export const RUSTFS_HOST = process.env['RUSTFS_HOST']!;
 export const DB_NAME = "miostream";
 export const DB_PASS = process.env["DB_PASS"] ?? await Bun.file(process.env["DB_PASS_FILE"]!).text();
 export const DB_URL = `postgresql://postgres:${DB_PASS}@${POSTGRES_HOST}/${DB_NAME}`;
+
+export const RUSTFS_ACCESS_KEY = process.env["RUSTFS_ACCESS_KEY"] ?? await Bun.file(process.env["RUSTFS_ACCESS_KEY_FILE"]!).text();
+export const RUSTFS_SECRET_KEY = process.env["RUSTFS_SECRET_KEY"] ?? await Bun.file(process.env["RUSTFS_SECRET_KEY_FILE"]!).text();
