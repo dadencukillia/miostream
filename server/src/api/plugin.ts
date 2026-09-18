2982b157714b52cde0fb4e9124fbe833a7f3dfc3
Maria <synevicmaria13@gmail.com>
2026-09-18 19:00:46 +0300

    feat(auth): scaffold authentication module

--- server/src/api/plugin.ts
+++ server/src/api/plugin.ts
@@ -1,7 +1,11 @@
-export default function(fastify, opts, done) {
+import type { FastifyPluginCallback } from "fastify";
+
+const plugin: FastifyPluginCallback = (fastify, _opts, done) => {
   fastify.get('/user', (request, reply) => {
     reply.send({ hello: 'world' });
   });
 
   done();
-}
+};
+
+export default plugin;
