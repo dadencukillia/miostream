df6b5a72b529cf8b8108f3f2172f482a1ca69eea
Illia Diadenchuk <dadencukillia@gmail.com>
2026-09-19 23:38:47 +0300

    refactor: shadcn, typescript, aliases adjustments

--- client/astro.config.mjs
+++ client/astro.config.mjs
@@ -1,11 +1,33 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
+import tailwindcss from '@tailwindcss/vite';
+import { fileURLToPath, URL } from 'node:url';
+import react from "@astrojs/react";
+
+const resolveBase = (path) => fileURLToPath(new URL(path, import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+
   adapter: node({
     mode: "standalone",
   }),
+
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@styles": resolveBase("./src/styles"),
+        "@components": resolveBase("./src/components"),
+        "@ui": resolveBase("./src/components/ui"),
+        "@lib": resolveBase("./src/lib"),
+        "@hooks": resolveBase("./src/hooks"),
+        "@utils": resolveBase("./src/lib/utils"),
+      },
+    },
+  },
+
+  integrations: [react()]
 });
