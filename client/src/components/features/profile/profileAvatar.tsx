4ff953c9b3fd84f03440f7a3d664a2c38d55a34c
Illia Diadenchuk <dadencukillia@gmail.com>
2026-09-21 02:12:26 +0300

    feat: two sections user profile page

--- client/src/components/features/profile/profileAvatar.tsx
+++ client/src/components/features/profile/profileAvatar.tsx
@@ -3,18 +3,21 @@ import { Avatar, AvatarImage, AvatarFallback } from "@ui/avatar";
 interface Props {
   username: string,
   imageUrl: string,
-  class: string
+  className?: string,
+  classImg?: string,
+  classFallback?: string
 }
 
 export default function ProfileAvatar({
-  username, imageUrl, class: className
+  username, imageUrl, className, classImg, classFallback
 }: Props) {
-  return (<Avatar>
-    <AvatarImage
-      src={ imageUrl }
-      alt={ "@" + username }
-      className={ className }
-    />
-    <AvatarFallback>{ username.trim().slice(0, 2).toUpperCase() }</AvatarFallback>
+  return (
+    <Avatar className={ className }>
+      <AvatarImage
+        src={ imageUrl }
+        alt={ "@" + username }
+        className={ classImg }
+      />
+      <AvatarFallback className={ classFallback }>{ username.trim().slice(0, 2).toUpperCase() }</AvatarFallback>
   </Avatar>);
 }
