e1e00031185d56c859de9695cb66fadf16b58e57
Illia Diadenchuk <dadencukillia@gmail.com>
2026-09-21 15:09:11 +0300

    feat: error catching via fastify

--- server/src/cdn/avatar/avatar.controller.ts
+++ server/src/cdn/avatar/avatar.controller.ts
@@ -13,30 +13,11 @@ export const getAvatarController = async (
 ) => {
   const { avatarId } = request.params;
 
-  try {
-    const buffer = await getAvatar(request.server.s3, request.log, avatarId);
-    return reply
-      .header("content-type", "image/webp")
-      .header("cache-control", "public, max-age=31536000, immutable")
-      .send(buffer);
-  } catch(e) {
-    if (e instanceof AvatarError) {
-      return reply
-        .status(e.status)
-        .send({
-          ok: false,
-          message: e.message,
-        });
-    }
-
-    request.log.error(e);
-    return reply
-      .status(500)
-      .send({
-        ok: false,
-        message: "unexpected error",
-      });
-  }
+  const buffer = await getAvatar(request.server.s3, request.log, avatarId);
+  return reply
+    .header("content-type", "image/webp")
+    .header("cache-control", "public, max-age=31536000, immutable")
+    .send(buffer);
 };
 
 export const resetAvatarController = async (
@@ -46,27 +27,8 @@ export const resetAvatarController = async (
   // TODO: replace on working auth middleware context
   const userId = "user";
 
-  try {
-    await resetUserAvatar(request.server.s3, db, request.log, userId);
-    return reply.send({ ok: true });
-  } catch(e) {
-    if (e instanceof AvatarError) {
-      return reply
-        .status(e.status)
-        .send({
-          ok: false,
-          message: e.message,
-        });
-    }
-
-    request.log.error(e);
-    return reply
-      .status(500)
-      .send({
-        ok: false,
-        message: "unexpected error",
-      });
-  }
+  await resetUserAvatar(request.server.s3, db, request.log, userId);
+  return reply.send({ ok: true });
 };
 
 export const updateAvatarController = async (
@@ -76,31 +38,12 @@ export const updateAvatarController = async (
   // TODO: replace on working auth middleware context
   const userId = "user";
 
-  try {
-    const result = await updateUserAvatarImage(
-      request.server.s3, db, 
-      request.log, 
-      userId, 
-      Buffer.from(request.body, "base64")
-    );
+  const result = await updateUserAvatarImage(
+    request.server.s3, db, 
+    request.log, 
+    userId, 
+    Buffer.from(request.body, "base64")
+  );
 
-    return reply.send(result);
-  } catch(e) {
-    if (e instanceof AvatarError) {
-      return reply
-        .status(e.status)
-        .send({
-          ok: false,
-          message: e.message,
-        });
-    }
-
-    request.log.error(e);
-    return reply
-      .status(500)
-      .send({
-        ok: false,
-        message: "unexpected error",
-      });
-  }
+  return reply.send(result);
 };
--- server/src/cdn/avatar/plugin.ts
+++ server/src/cdn/avatar/plugin.ts
@@ -1,8 +1,28 @@
 import type { FastifyInstance } from "fastify";
 import { getSchema, resetSchema, updateSchema } from "./schemas";
 import { getAvatarController, resetAvatarController, updateAvatarController } from "./avatar.controller";
+import { AvatarError } from "./avatar.service";
 
 export default function(fastify: FastifyInstance, _opts: {}, done: () => void) {
+  fastify.setErrorHandler<Error>((error, request, reply) => {
+    if (error instanceof AvatarError) {
+      return reply
+        .status(error.status)
+        .send({
+          ok: false,
+          message: error.message,
+        });
+    }
+
+    request.log.error(error);
+    return reply
+      .status(500)
+      .send({
+        ok: false,
+        message: error.message
+      });
+  });
+
   fastify.get('/:avatarId(^[a-zA-Z0-9]{32}).webp', { schema: getSchema }, getAvatarController);
 
   // TODO: wrap auth middleware
